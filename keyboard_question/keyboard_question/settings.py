52d3365a4817e454cb52c23584a47a9c780170b9
Catalyst-42 <nikis16042004@gmail.com>
2026-01-16 20:37:41 +0300

    Supabase ssl require

--- keyboard_question/keyboard_question/settings.py
+++ keyboard_question/keyboard_question/settings.py
@@ -133,6 +133,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {  # Supabase
+            'sslmode': 'require',
+       }
     }
 }
 
